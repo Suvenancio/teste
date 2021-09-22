39d20dc8607533d5ecd0987a539b2d421527056e
Suvenancio <venancisuh@gmail.com>
2021-09-22 18:46:33 -0300

    deploy

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 1000;
+const port = process.env.PORT;
 
 const bd = require('./infra/sqlite_bd');
 const cors = require('cors');
